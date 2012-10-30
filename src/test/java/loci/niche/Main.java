0e99e7e60af0488f31b6f146f398937fe04a2764
Curtis Rueden <ctrueden@wisc.edu>
2012-10-30 17:10:02 -0500

    Add a simple test class for the MODS form
    We add ij-app to the test classpath so that when the Main method is run,
    all the standard services are available to its ImageJ context.

---
+++ src/test/java/loci/niche/Main.java
@@ -0,0 +1,49 @@
+/*
+ * #%L
+ * Support code for the 3D Niche at the Wisconsin Institutes for Discovery.
+ * %%
+ * Copyright (C) 2012 Board of Regents of the University of Wisconsin-Madison.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package loci.niche;
+
+import imagej.ImageJ;
+import imagej.command.CommandService;
+import imagej.ui.UIService;
+
+/**
+ * Simple test class for the {@link MODSForm} command.
+ * 
+ * @author Curtis Rueden
+ */
+public class Main {
+
+	public static void main(final String... args) {
+		final ImageJ context = ImageJ.createContext();
+
+		// show the UI
+		final UIService uiService = context.getService(UIService.class);
+		uiService.createUI();
+
+		// display the MODS form
+		final CommandService commandService =
+			context.getService(CommandService.class);
+		commandService.run(MODSForm.class);
+	}
+
+}
